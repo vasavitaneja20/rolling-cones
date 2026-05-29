357dbb7017ae1f0b3acf332d3cc595a5d293404d
Ashmit-A <ashmit.arora@vwo.com>
2026-05-29 22:10:58 +0530

    socket path fix

--- client/src/sockets/socket.js
+++ client/src/sockets/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL, {
-    transports: ["websocket", "polling"],
-  });
+const BASE_URL = import.meta.env.VITE_API_URL.replace("/api", "");
+
+const socket = io(BASE_URL, {
+  transports: ["websocket", "polling"],
+});
 
 export default socket;
