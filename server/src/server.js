7b0beb37c9fe61f990fd6b1de67c3169ad5825ef
Ashmit-A <ashmit.arora@vwo.com>
2026-05-29 19:32:16 +0530

    vercel.json added

--- server/src/server.js
+++ server/src/server.js
@@ -46,7 +46,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST","PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
 
